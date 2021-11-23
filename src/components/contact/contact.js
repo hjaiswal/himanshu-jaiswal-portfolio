a67c3bcdf6b9033e4b6f716b015c582856ce9e2a
hjaiswal <jaiswalhimanshu.36@gmail.com>
2021-11-24 00:27:56 +0530

    changes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -44,6 +44,12 @@ export default class contact extends Component {
         </section>{" "}
         <footer>
           <div className="row">
+            <div className="col-six tab-full">
+              <div className="copyright">
+                <span>© Copyright Kards 2016.</span> 
+                <span>Design by <a href="http://www.styleshout.com/">styleshout</a></span>	         	
+              </div>		                  
+            </div>
             <div id="go-top">
               <a className="smoothscroll" title="Back to Top" href="#top"><i className="fa fa-long-arrow-up" /></a>
             </div>
