f472c1dc60d487b3ed8ea2e31f19956a5b210c22
hjaiswal <jaiswalhimanshu.36@gmail.com>
2021-11-23 23:42:06 +0530

    Updates

---
+++ js/smooth.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+  $('.smoothscroll').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+var toggleButton = $('.menu-toggle'),
+       nav = $('.main-navigation');
+
+   // toggle button
+   toggleButton.on('click', function(e) {
+
+		e.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.slideToggle();
+
+	});
+});
